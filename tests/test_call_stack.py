73bfda811a2c0c59c86aa36bbc7cfc67d038848f
mewennl <mwnl@sas.upenn.edu>
2026-06-25 11:17:16 -0400

    simple recursion visualizer, preparing for actual working model in v5

--- tests/test_call_stack.py
+++ tests/test_call_stack.py
@@ -25,3 +25,30 @@ class TestCallStack:
         with pytest.raises(IndexError):
             call_stack.return_(return_value=None)
 
+    def test_get_frames_on_empty(self):
+        call_stack = CallStack()
+        frames = call_stack.get_frames()
+        assert frames == []
+
+    def test_get_frames_number_of_returns(self):
+        call_stack = CallStack()
+        call_stack.call(function_name="add", arguments=(1, 2), local_variables={"x": 1, "y": 2})
+        num_frames = len(call_stack.get_frames())
+        assert num_frames == 1
+
+    def test_get_frames_order(self):
+        call_stack = CallStack()
+        call_stack.call(function_name="first", arguments=(1,), local_variables={"a":1})
+        call_stack.call(function_name="second", arguments=(2,), local_variables={"b":2})
+        call_stack.call(function_name="third", arguments=(3,), local_variables={"c":3})
+        frames = call_stack.get_frames()
+        assert frames[0].function_name == "first"
+        assert frames[1].function_name == "second"
+        assert frames[2].function_name == "third"
+
+    def test_get_frames_after_return(self):
+        call_stack = CallStack()
+        call_stack.call(function_name="first", arguments=(1,), local_variables={"a":1})
+        assert call_stack.get_frames() != []
+        call_stack.return_(return_value=3)
+        assert call_stack.get_frames() == []
