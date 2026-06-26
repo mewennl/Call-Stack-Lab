f07c233346108791975b99292c8f42254f25fc3d
mewennl <mwnl@sas.upenn.edu>
2026-06-26 13:21:15 -0400

    fix: missing content files

--- src/call_stack/call_stack.py
+++ src/call_stack/call_stack.py
@@ -25,3 +25,13 @@ class CallStack:
             frame_list.append(stack_frame)
         return frame_list
 
+    def raise_exception(self, exception: Exception, message: str) -> str:
+        frame_list = self.get_frames()
+        lines = ["Traceback (most recent call last):"]
+        for frame in frame_list:
+            lines.append(f" in {frame.function_name}{frame.arguments}")
+        while not self.stack.is_empty():
+            self.stack.pop()
+
+        lines.append(f"{exception.__name__}: {message}")
+        return "\n".join(lines)
